e80e0f6f595b83ba5c8cff8cebb2546ed119c78d
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-04-29 12:28:32 -0400

    Next Gen

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+const userName = "Max";
+// userName = 'Chris';
+let age = 30;
+
+age = 29;
+
+const add = (a: number, b: number = 1) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput = (output: string | number) => {
+  console.log(output);
+};
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
