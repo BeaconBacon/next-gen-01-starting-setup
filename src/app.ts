d1af8c86dd2605590ccc1000e5ab9203bc80617f
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-04-30 08:53:44 -0400

    Updates

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,25 @@ const add = (a: number, b: number = 1) => a + b;
 
 console.log(add(2, 5));
 
-const printOutput = (output: string | number) => {
+const printOutput: (a: number | string) => void = (output) =>
   console.log(output);
-};
+
 const button = document.querySelector("button");
 
 if (button) {
   button.addEventListener("click", (event) => console.log(event));
 }
+
+printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: "Max",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
